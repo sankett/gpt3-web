234b46d188979f8516c316dab091ced0b7a04580
Sanket Terdal <Sanket.Terdal@blueconchtech.com>
2022-12-27 12:24:29 +0530

    changes config

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="ChatGPT - OpenAI" key="title"/>
+        <meta property="og:description" content="This a demo of ChatGPT capabilities" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://storybook7.blob.core.windows.net/images/ai.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
-        <title>Sanket's GPT3 demo</title>
       </Head>
       <body>
         <Main />
