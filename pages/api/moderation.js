393612511d4e4b96521f08b40cfd4f409cf9209a
sanket <sanket.terdal@gmail.com>
2023-01-25 22:20:40 +0530

    chg6

--- pages/api/moderation.js
+++ pages/api/moderation.js
@@ -13,7 +13,7 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ['OPTIONS','GET','POST',],
+    methods: ['OPTIONS'],
   })
 );
 
